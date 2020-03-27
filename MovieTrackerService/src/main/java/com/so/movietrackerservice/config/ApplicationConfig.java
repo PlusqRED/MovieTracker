b6021018d298aab8f463800e503bf5cca982c8f4
Oleg <36641727+plusqred@users.noreply.github.com>
2020-03-27 21:52:21 +0300

    Swagger 2 was implemented

--- MovieTrackerService/src/main/java/com/so/movietrackerservice/config/ApplicationConfig.java
+++ MovieTrackerService/src/main/java/com/so/movietrackerservice/config/ApplicationConfig.java
@@ -9,10 +9,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableSwagger2
 public class ApplicationConfig {
 
     @Value("${bot.token}")
@@ -35,5 +41,14 @@ public class ApplicationConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
